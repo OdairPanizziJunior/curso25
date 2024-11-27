0698079c8d413ee5cdbec7e421d9427387c30fba
OdairPanizziJunior <odair@Notebook-Dell-O>
2024-11-26 21:36:04 -0300

    Variáveis me PHP

---
+++ PersonalProjects/variaveis.php
@@ -0,0 +1,22 @@
+<?php
+
+//Constantes (sempre maiúscula, com _ se necesário)
+const CPF = '01234567890';
+const VEL_LUZ = '320k\s2'; 
+
+//tipos de dados primitivos do PHP.
+$inteiro = 2;
+$string = "Um texto qualquer";
+$nomePessoa = "Odair"; //style guide Camel Case
+$nome_pessoa = "Odair"; //style guide Snake Case
+$float = 2.5;
+$double = 2.5468;
+$char = 'a';
+
+//tipos de dados estruturais
+$array = array();
+$array_short = [];
+$objeto = new stdClass();
+$classe = new stdClass();
+
+?>
