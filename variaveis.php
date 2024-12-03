9040f404a18f45c905694256cdc1d511f0e9b5c7
OdairPanizziJunior <odair@Notebook-Dell-O>
2024-12-03 19:40:07 -0300

    Update

--- variaveis.php
+++ variaveis.php
@@ -5,13 +5,13 @@ const CPF = '01234567890';
 const VEL_LUZ = '320k\s2'; 
 
 //tipos de dados primitivos do PHP.
-$inteiro = 2;
-$string = "Um texto qualquer";
-$nomePessoa = "Odair"; //style guide Camel Case
-$nome_pessoa = "Odair"; //style guide Snake Case
-$float = 2.5;
-$double = 2.5468;
-$char = 'a';
+string $inteiro = 2;
+string $string = "Um texto qualquer";
+string $nomePessoa = "Odair"; //style guide Camel Case
+string $nome_pessoa = "Odair"; //style guide Snake Case
+float $float = 2.5;
+double $double = 2.5468;
+string $char = 'a';
 
 //tipos de dados estruturais
 $array = array();
