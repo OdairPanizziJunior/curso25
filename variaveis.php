4e6f66122ebdbd9e8da932f9eb70d5bba9f97e8e
OdairPanizziJunior <odair@Notebook-Dell-O>
2024-12-03 20:05:41 -0300

    Update

--- variaveis.php
+++ variaveis.php
@@ -5,20 +5,36 @@ const CPF = '01234567890';
 const VEL_LUZ = '320k\s2'; 
 
 //tipos de dados primitivos do PHP.
-int $inteiro = 2;
-string $string = "Um texto qualquer";
-string $nomePessoa = "Odair"; //style guide Camel Case
-string $nome_pessoa = "Odair"; //style guide Snake Case
-float $float = 2.5;
-double $double = 2.5468;
-string $char = 'a';
+$inteiro = 2;
+$string = "Um texto qualquer";
+$nomePessoa = "Odair"; //style guide Camel Case
+$nome_pessoa = "Odair"; //style guide Snake Case
+$float = 2.5;
+$double = 2.5468;
+$char = 'a';
 
 //Obs: o PHP já entende o tipo de dado colocado, então não necessáriamente precisa declarar o tipo. 
 
-//tipos de dados estruturais
-$array = array();
-$array_short = [];
-$objeto = new stdClass();
+//tipos de dados estruturais (Objeto ou Classe)
+$array = array(); //versões antigas do PHP 5.0 - 7.0
+$array_short = []; //versões mais novas do PHP 7.0+
+$objeto = new stdClass(); //POO - OOP
 $classe = new stdClass();
+
+//Exemplo de uma Classe
+class CanetaAzul { // nome do objeto
+    // atributos e/ou propriedades
+    public const PLASTICO = true;
+    public const COR = "Azul";
+    public $nome;
+    public $tipoMaterial = "Plastico";
+    public $dimensoes;
+    public $tipo;
+
+    // metodos e/ou funções (sempre no infinitivo)
+    public function escrever(){
+
+    }
+}
 
 ?>
