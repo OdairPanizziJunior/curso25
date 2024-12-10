b45b8c3407af6c590d11a70a4c252e14ff7400b5
OdairPanizziJunior <odair@Notebook-Dell-O>
2024-12-10 19:08:22 -0300

    Update

--- tabuada.php
+++ tabuada.php
@@ -17,7 +17,7 @@
                 echo "<h2>Tabuada do $numero</h2>";
                 echo "<ul>";
 
-                for ($i = 2; $i <= 10; $i++) {
+                for ($i = 1; $i <= 10; $i++) {
                     $resultado = $numero * $i;
                     echo "<li>$numero x $i = $resultado</li>";
                 }
