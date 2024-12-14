fb92ffe040fbb5e597cce785f3cc380d1756b28c
odairpanizzijunior <odairpanizzijunior@gmail.com>
2024-12-14 19:00:02 -0300

    Update

--- primos.php
+++ primos.php
@@ -4,51 +4,93 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Checagem de Números Primos</title>
-    <style>
-        div.form{
+   <style>
+        body {
+            background-color: #f8f9fa;
+            font-family: Arial, sans-serif;
+        }
+        .form {
+            max-width: 550px;
+            margin: 0 auto;
             background-color: #ffffff;
-            border: 1px solid #ccc;
-            border-radius: 8px;
-            padding: 20px;
+            padding: 30px;
+            border-radius: 10px;
             box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-            width: 500px;
+        }
+        h1 {
+            font-size: 30px !important;
+            font-weight: bold;
+            color: #007bff;
+        }
+        h2 {
+            font-size: 20px !important;;
+            color: #6c757d;
+            margin-bottom: 20px;
             text-align: center;
         }
+        input[type="number"] {
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 20px;
+            border: 1px solid #ced4da;
+            border-radius: 5px;
+            font-size: 1rem;
+        }
+        input[type="submit"] {
+            width: 100%;
+            padding: 10px;
+            background-color: #007bff;
+            color: white;
+            border: none;
+            border-radius: 20px;
+            font-size: 30px;
+            cursor: pointer;
+            transition: background-color 0.3s;
+        }
+        input[type="submit"]:hover {
+            background-color: #0056b3;
+        }
     </style>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <div class="form">    
+<div class="form">
         <h1>Quer saber se o número é Primo?</h1>
         <h2>Digite-o e clique em testar</h2>
         <form action="" method="POST">
-            <input type="number" name="number" required />
+            <input type="number" name="number" required placeholder="Digite o número aqui" />
             <input type="submit" value="Testar" />
         </form>
     </div>
     <br>
-<?php
-//Descobrir se o número é primo ou não.
-$numero = $_POST['number'];
+    <div class="form">
+        <?php
+        //Descobrir se o número é primo ou não.
 
-if($numero <= 1) {
-    echo "Números menores ou iguais a 1 não são considerados primos.";    
-    exit;
-}
+        $numero = $_POST['number'] ?? null;
 
-$divisores = 0;
+        if($numero <= 1) {
+            echo "Números menores ou iguais a 1 não são considerados primos.";    
+            exit;
+        }
 
-    for($i = 2; $i < $numero; $i++){
-        if ($numero % $i == 0){ //$i é o divisor (dividindo sempre por 2)
-        echo $numero . " pode ser divido por $i.<br />"; //quais números dividiram $numero. 
-        $divisores++;
-        } 
-    }
-    if ($divisores == 0) {
-        echo "É primo!";
-    } else {
-        echo "Portanto " .$numero . " não é primo!<br> Ele possui $divisores divisor(es) além de 1 e ele mesmo.";
-    }
-?>
+        $divisores = 0;
+
+        for($i = 2; $i < $numero; $i++){
+            if ($numero % $i == 0){ //$i é o divisor (dividindo sempre por 2)
+                echo $numero . " pode ser divido por $i.<br />"; //quais números dividiram $numero. 
+                $divisores++;
+                } 
+            }
+            if ($divisores == 0) {
+                echo "É primo!";
+            } else {
+                echo "Portanto " .$numero . " não é primo!<br> Ele possui $divisores divisor(es) além de 1 e ele mesmo.";
+            }
+        ?>
+</div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 </html>
