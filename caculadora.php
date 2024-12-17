4e776b5fed9221218a03477ba3e492dd1cfb7283
odairpanizzijunior <odairpanizzijunior@gmail.com>
2024-12-17 20:49:32 -0300

    Update

--- caculadora.php
+++ caculadora.php
@@ -1,18 +1,18 @@
 <?php
 //Calculadora
 
-$n1 = 10;
-$n2 = 2;
+$numero1 = 10;
+$numero2 = 2;
 $operador = "*";
 
 if ($operador == "+"){
-    echo "A soma de " . $n1 . " + " . $n2 . " é igual a " . ($n1 + $n2);
+    echo "A soma de " . $numero1 . " + " . $numero2 . " é igual a " . ($numero1 + $numero2);
 }elseif($operador == "-"){
-    echo "A subtração de " . $n1 . " - " . $n2 . " é igual a " . ($n1 - $n2);
+    echo "A subtração de " . $numero1 . " - " . $numero2 . " é igual a " . ($numero1 - $numero2);
 }elseif($operador == "*"){
-    echo "A multiplicação de " . $n1 . " x " . $n2 . " é igual a " . ($n1 * $n2);
+    echo "A multiplicação de " . $numero1 . " x " . $numero2 . " é igual a " . ($numero1 * $numero2);
 }elseif($operador == "/"){
-    echo "A divisão de " . $n1 . " / " . $n2 . " é igual a " . ($n1 / $n2);
+    echo "A divisão de " . $numero1 . " / " . $numero2 . " é igual a " . ($numero1 / $numero2);
 }else  {
     echo "Somente as operações: <br> + <br> - <br> * <br>/";
 }
