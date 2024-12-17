66c02fd1b40843a094e635b1b6b6f1745e55710f
odairpanizzijunior <odairpanizzijunior@gmail.com>
2024-12-17 20:25:09 -0300

    Update

---
+++ caculadora.php
@@ -0,0 +1,18 @@
+<?php
+//Calculadora
+
+$n1 = 10;
+$n2 = 2;
+$operador = "e";
+
+if ($operador == "+"){
+    echo "A soma de " . $n1 . " + " . $n2 . " é igual a " . ($n1 + $n2);
+}elseif($operador == "-"){
+    echo "A subtração de " . $n1 . " - " . $n2 . " é igual a " . ($n1 - $n2);
+}elseif($operador == "*"){
+    echo "A multiplicação de " . $n1 . " x " . $n2 . " é igual a " . ($n1 * $n2);
+}elseif($operador == "/"){
+    echo "A divisão de " . $n1 . " / " . $n2 . " é igual a " . ($n1 / $n2);
+}else  {
+    echo "Somente as operações: <br> + <br> - <br> * <br>/";
+}
