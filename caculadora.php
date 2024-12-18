4e309118c8a862f02ab8f19b0d739b377de35fea
odairpanizzijunior <odairpanizzijunior@gmail.com>
2024-12-17 21:03:45 -0300

    Update

--- caculadora.php
+++ caculadora.php
@@ -1,9 +1,9 @@
 <?php
 //Calculadora
 
-$numero1 = 10;
+$numero1 = 50;
 $numero2 = 2;
-$operador = "*";
+$operador = "#";
 
 if ($operador == "+"){
     echo "A soma de " . $numero1 . " + " . $numero2 . " é igual a " . ($numero1 + $numero2);
@@ -13,6 +13,8 @@ if ($operador == "+"){
     echo "A multiplicação de " . $numero1 . " x " . $numero2 . " é igual a " . ($numero1 * $numero2);
 }elseif($operador == "/"){
     echo "A divisão de " . $numero1 . " / " . $numero2 . " é igual a " . ($numero1 / $numero2);
-}else  {
+}else if($operador == "#") {
+    echo  $numero2 . " %  de " . $numero1 . " é igual a " . ($numero1 * ($numero2/100));
+}else{
     echo "Somente as operações: <br> + <br> - <br> * <br>/";
 }
