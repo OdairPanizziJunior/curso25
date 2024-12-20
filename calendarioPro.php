f085f60a2f27e2f7a9eee4103cf30a0231d3a8c4
Odair Panizzi Júnior <101676143+OdairPanizziJunior@users.noreply.github.com>
2024-12-20 09:42:08 -0300

    Update calendarioPro.php

--- calendarioPro.php
+++ calendarioPro.php
@@ -4,13 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calendário PHP</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
     <form method="POST">
-        <label>Digite uma Data</label>
+        <label class="form-label mt-4">Digite uma Data</label>
         <input type="text" name="data" placeholder="DD/MM/AAAA" id="data">
-        <button type="submit">Testar</button>
+        <button type="submit" class="btn btn-primary">Testar</button><br>
     </form>
+
 <?php
     // criar um algoritmo que simula o funcionamento de um calendário.
     // variáveis: $data = '31/01/2025'
@@ -45,6 +47,9 @@
     echo "Data: " . $data;
     echo "<br>Muito bem!"
 ?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </body>
 </html>
+
 
