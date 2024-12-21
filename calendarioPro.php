d3a37803cc7106e14422b0fd978602abea04a8bf
Odair Panizzi Júnior <odairpanizzijunior@gmail.com>
2024-12-21 19:10:00 -0300

    Update calendarioPro.php

--- calendarioPro.php
+++ calendarioPro.php
@@ -7,46 +7,47 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <form method="POST">
-        <label class="form-label mt-4">Digite uma Data</label>
-        <input type="text" name="data" placeholder="DD/MM/AAAA" id="data">
-        <button type="submit" class="btn btn-primary">Testar</button><br>
-    </form>
+    <div class="container">
+        <form method="POST">
+            <label class="form-label mt-4">Digite uma Data</label>
+            <input type="text" name="data" placeholder="DD/MM/AAAA" id="data">
+            <button type="submit" class="btn btn-primary">Testar</button><br>
+        </form>
+    </div>
+    <?php
+        // criar um algoritmo que simula o funcionamento de um calendário.
+        // variáveis: $data = '31/01/2025'
+        // Validar a data
+        // validar se o ano é bissexto
+        // Dica IF else
 
-<?php
-    // criar um algoritmo que simula o funcionamento de um calendário.
-    // variáveis: $data = '31/01/2025'
-    // Validar a data
-    // validar se o ano é bissexto
-    // Dica IF else
+        $data = $_POST['data'] ?? null;  
 
-    $data = $_POST['data'] ?? null;  
-
-    
-    if($data == null) {
-        echo "Digite uma data para testar";    
-        exit;
-    }
+        
+        if($data == null) {
+            echo "Digite uma data para testar";    
+            exit;
+        }
 
 
-    function validaFormatoData($data) {
-        return preg_match('/^\d{2}\/\d{2}\/\d{4}$/', $data);
-    }
+        function validaFormatoData($data) {
+            return preg_match('/^\d{2}\/\d{2}\/\d{4}$/', $data);
+        }
 
-    if (!validaFormatoData($data)) {
-        echo "Formato inválido, use o formato DD/MM/AAAA. <br>Você digitou: " . $data . ".<br>";
-        exit;
-    }
+        if (!validaFormatoData($data)) {
+            echo "Formato inválido, use o formato DD/MM/AAAA. <br>Você digitou: " . $data . ".<br>";
+            exit;
+        }
 
-    list($dia, $mes, $ano) = explode('/', $data);
+        list($dia, $mes, $ano) = explode('/', $data);
 
-    if (!checkdate((int)$mes, (int)$dia, (int)$ano)) {
-        echo "A data $data não é válida no calendário!<br>";
-        exit;
-    }
-    echo "Data: " . $data;
-    echo "<br>Muito bem!"
-?>
+        if (!checkdate((int)$mes, (int)$dia, (int)$ano)) {
+            echo "A data $data não é válida no calendário!<br>";
+            exit;
+        }
+        echo "Data: " . $data;
+        echo "<br>Muito bem!"
+    ?>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </body>
