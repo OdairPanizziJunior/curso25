010c57df60f4101f736192ce47770bb8c4878b41
odairpanizzijunior <odairpanizzijunior@gmail.com>
2025-01-21 21:57:59 -0300

    Update

---
+++ ContaBancaria.php
@@ -0,0 +1,19 @@
+<?php
+
+class ContaBancaria{
+
+    public $titular = '';
+    public $saldo = 0;
+    public $destinatario = '';
+    public $historico = [];
+
+
+    public function sacar(){}
+    public function depositar(){}
+    public function pix(){}
+    public function verSaldo(){}
+    public function estrato(){}
+
+}
+
+
