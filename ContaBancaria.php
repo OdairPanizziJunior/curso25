575a073b1c9d19089dbc993d75df670e46a28804
odairpanizzijunior <odairpanizzijunior@gmail.com>
2025-01-28 18:22:50 -0300

    update

--- ContaBancaria.php
+++ ContaBancaria.php
@@ -8,6 +8,13 @@ class ContaBancaria{
     public $historico = [];
 
 
+    /* Construtor que inicializa o Titular e saldo inicial */
+    public function __construct($titular, $saldoInicial = 0) {
+    $this->titular = $titular;
+    $this->saldo = $saldoInicial;
+}
+
+
     public function sacar(){}
     public function depositar(){}
     public function pix(){}
